ac3b6da87b8440ae1bc30cd5a017cf6306bc8762
MegaBarf <125373879+MegaBarf@users.noreply.github.com>
2025-03-24 13:40:55 +0100

    Update ep2000.py

--- custom_components/bluetti_bt/bluetti_bt_lib/devices/ep2000.py
+++ custom_components/bluetti_bt/bluetti_bt_lib/devices/ep2000.py
@@ -1,1 +1,9 @@
+"""EP2000 fields."""
+
+from ..base_devices.ProtocolV2Device import ProtocolV2Device
+
+
+class EP2000(ProtocolV2Device):
+    def __init__(self, address: str, sn: str):
+        super().__init__(address, "EP2000", sn)
 
