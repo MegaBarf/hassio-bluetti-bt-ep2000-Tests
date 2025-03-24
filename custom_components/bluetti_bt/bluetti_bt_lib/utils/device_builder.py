edc24e40fbf9efffdb1dc4a42d5cad82d9057931
MegaBarf <125373879+MegaBarf@users.noreply.github.com>
2025-03-24 13:43:26 +0100

    Update device_builder.py

--- custom_components/bluetti_bt/bluetti_bt_lib/utils/device_builder.py
+++ custom_components/bluetti_bt/bluetti_bt_lib/utils/device_builder.py
@@ -12,14 +12,16 @@ from ..devices.ac200m import AC200M
 from ..devices.ac300 import AC300
 from ..devices.ac500 import AC500
 from ..devices.eb3a import EB3A
+from ..devices.ep800 import EP2000
 from ..devices.ep500 import EP500
 from ..devices.ep500p import EP500P
 from ..devices.ep600 import EP600
 from ..devices.ep760 import EP760
 from ..devices.ep800 import EP800
 
+
 DEVICE_NAME_RE = re.compile(
-    r"^(AC60|AC70|AC70P|AC180|AC180P|AC200L|AC200M|AC300|AC500|EB3A|EP500|EP500P|EP600|EP760|EP800)(\d+)$"
+    r"^(AC60|AC70|AC70P|AC180|AC180P|AC200L|AC200M|AC300|AC500|EB3A|EP2000|EP500|EP500P|EP600|EP760|EP800)(\d+)$"
 )
 
 
@@ -45,6 +47,8 @@ def build_device(address: str, name: str):
         return AC500(address, match[2])
     if match[1] == "EB3A":
         return EB3A(address, match[2])
+    if match[1] == "EP2000":
+        return EP2000(address, match[2])
     if match[1] == "EP500":
         return EP500(address, match[2])
     if match[1] == "EP500P":
