f45c49efbddd0e455102a84059a55d067fba5520
MegaBarf <125373879+MegaBarf@users.noreply.github.com>
2025-05-28 20:22:29 +0200

    Update ep2000.py

--- custom_components/bluetti_bt/bluetti_bt_lib/devices/ep2000.py
+++ custom_components/bluetti_bt/bluetti_bt_lib/devices/ep2000.py
@@ -7,9 +7,6 @@ class EP2000(ProtocolV2Device):
     def __init__(self, address: str, sn: str):
         super().__init__(address, "EP2000", sn)
 
-
-
-
-
-
-#         self.struct.add_uint_field("total_battery_percent_base_device", 102)
+        # Details
+       # self.struct.add_uint_field("battery_range_start", 2022) ##bsp. aus der EO600.py
+        self.struct.add_uint_field("total_battery_percent_ep2000_py", 102)
